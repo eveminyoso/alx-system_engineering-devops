75539fe96b4fc50567c800745de0ebccf204dc97
eveminyoso <eveminyoso@gmail.com>
2024-01-04 13:58:37 -0800

    0x06-regular_expressions

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#
+# Checks for arguments
+if ARGV.empty?
+  puts "Usage: ruby match_patterns.rb <input>"
+else
+  # Obtain the argument
+  input = ARGV[0]
+
+  # Define the pattern
+  pattern = /^(?!hbo)/
+
+  matches = input.scan(pattern).flatten
+
+  if matches.any?
+    puts input
+  else
