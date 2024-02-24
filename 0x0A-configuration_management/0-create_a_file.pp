3688e86466f39c716b7749bd48e0610d79607423
eveminyoso <eveminyoso@gmail.com>
2024-02-23 22:41:36 -0800

    bug fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Create a file in /tmp
 
-# file { '/tmp/school':
-	   ensure  => 'file',
-	   mode    => '0744',
-	   owner   => 'www-data',
-	   group   => 'www-data',
-	   content => 'I love Puppet',
+file { '/tmp/school':
+        ensure  => 'file',
+        mode    => '0744',
+        owner   => 'www-data',
+        group   => 'www-data',
+        content => 'I love Puppet',
 }
