330407bbb8a4d46c5289f298864f66aef74eff2e
eveminyoso <eveminyoso@gmail.com>
2024-02-23 22:54:40 -0800

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/puppet
 # Install a specific version of flask (2.1.0)
 
-Package	{ 'flask':
-	Ensure   => '2.1.0',
-	Provider => 'pip3',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
